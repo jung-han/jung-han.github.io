14de09507951359e791616fd1d4d450d5194d48f
jung-han <hanjung@nhnent.com>
2018-07-14 21:59:02 +0900

    Updates

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: "Hanjung"
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-};
--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => {
-  return (
-    <footer id="footer" className="footer">
-      <p>© Copyright 2018 Hanjung</p>
-    </footer>
-  );
-};
