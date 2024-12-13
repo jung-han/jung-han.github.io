ca72ef300ed1d4a36282712915368da5ad67f62b
hanjung <hanjung@linecorp.com>
2024-12-13 19:34:22 +0900

    feat: 초안완료

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jung-han.github.io",
+  integrations: [tailwind()],
+  devToolbar: {
+    enabled: false,
+  },
 });
